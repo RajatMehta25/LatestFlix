ff433a00fb05956678aa6554fe4bf3c292bf826b
RajatMehta <rajat.mehta@codeaegis.in>
2024-09-29 11:52:54 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [search, onChangeSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [iframeLink, setIframeLink] = useState("");
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/RajatMehta25/TV/main/Movie.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+        onChangeSearch("");
+      });
+  }, []);
+
+  const searchMovie = () => {
+    let newData = data?.filter((ele) => ele.name.toLowerCase().includes(search.toLowerCase()));
+
+    // setFilteredData(newData ?? data);
+    return newData;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Outer">
+      <div className="margin">
+        <input
+          onChange={(e) => {
+            // if (e) {
+            onChangeSearch(e.target.value);
+            // searchChannel(e);
+            // } else {
+            // onChangeSearch(e);
+            // setFilteredData(data);
+            // }
+          }}
+          value={search}
+          placeholder="Search Content Name"
+        />
+      </div>
+      <div className="text">Download Links will work for 2 days only , after posting link</div>
+
+      <div className="CardsContainer">
+        {searchMovie(data)?.map((ele) => (
+          <div className="MovieCard">
+            <img src={ele.image} style={{ border: "1px solid white", borderRadius: "15px" }} width="200" height="200" />
+            <div className="text">{ele.name}</div>
+            <button>
+              <a style={{ textDecoration: "none", color: "white" }} href={ele.downloadLink}>
+                Download
+              </a>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
